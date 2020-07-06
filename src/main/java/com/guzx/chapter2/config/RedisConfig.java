ef5c2e149c96589d8b3aaf19818e706447cda842
hnguzx <2544697696@qq.com>
2020-07-06 13:07:15 +0800

    redis，记得改端口

--- src/main/java/com/guzx/chapter2/config/RedisConfig.java
+++ src/main/java/com/guzx/chapter2/config/RedisConfig.java
@@ -1,11 +1,42 @@
 package com.guzx.chapter2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import redis.clients.jedis.JedisPoolConfig;
 
 @Configuration
 public class RedisConfig {
     private RedisConnectionFactory connectionFactory = null;
+
+    @Bean(name = "RedisConnectionFactory")
+    public RedisConnectionFactory initRedisConnectionFactory() {
+        if (this.connectionFactory == null) {
+            return this.connectionFactory;
+        }
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxIdle(30);
+        poolConfig.setMaxTotal(50);
+        poolConfig.setMaxWaitMillis(1000);
+        // 创建jedis连接工厂
+        JedisConnectionFactory connectionFactory = new JedisConnectionFactory(poolConfig);
+        // 获取单机的redis配置
+        RedisStandaloneConfiguration redisStandaloneConfiguration = connectionFactory.getStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName("127.0.0.1");
+        redisStandaloneConfiguration.setPort(1234);
+        this.connectionFactory = connectionFactory;
+        return connectionFactory;
+    }
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<Object,Object> initRedisTemplate(){
+        RedisTemplate<Object,Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(this.connectionFactory);
+        return template;
+    }
 
 
 }
