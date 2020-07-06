a8d7ae72659216c454247c74d10d9d609a51e1fb
hnguzx <2544697696@qq.com>
2020-07-06 12:05:36 +0800

    mybatis

---
+++ src/main/java/com/guzx/chapter2/config/RedisConfig.java
@@ -0,0 +1,11 @@
+package com.guzx.chapter2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+    private RedisConnectionFactory connectionFactory = null;
+
+
+}
