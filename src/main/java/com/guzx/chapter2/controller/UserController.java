ce60e3b3526ff9534b009451ee3a51095cca840b
hnguzx <2544697696@qq.com>
2020-07-22 12:01:34 +0800

    重定向

--- src/main/java/com/guzx/chapter2/controller/UserController.java
+++ src/main/java/com/guzx/chapter2/controller/UserController.java
@@ -11,8 +11,10 @@ import com.guzx.chapter2.service.MyBatisUserService;
 import com.guzx.chapter2.service.impl.JdbcTemplImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import java.util.HashMap;
@@ -136,9 +138,66 @@ public class UserController {
     @GetMapping("/{id}")
     @ResponseBody
     // @PathVariable通过名称获取参数
-    public User getUser5(@PathVariable("id") Integer id){
+    public User getUser5(@PathVariable("id") Integer id) {
         User user = jdbcTempl.getUser2(id);
         return user;
     }
+
+    @GetMapping("/show")
+    public String showUser(Long id, Model model) {
+        User_MyBatis user_myBatis = myBatisUserService.getUser(id);
+        model.addAttribute("user", user_myBatis);
+        return "data/user";
+    }
+
+    @GetMapping("/show")
+    public String showUser2(User_MyBatis user_myBatis, Model model) {
+        System.out.println(user_myBatis.getUserName());
+        return "data/user";
+    }
+
+    @GetMapping("/redirect1")
+    public String redirect(String userName, String note) {
+        User_MyBatis user_myBatis = new User_MyBatis();
+        user_myBatis.setUserName(userName);
+        user_myBatis.setNote(note);
+        myBatisUserService.insertUser(user_myBatis);
+        return "redirect:/user/show?id=" + user_myBatis.getId();
+    }
+
+    @GetMapping("/redirect2")
+    public ModelAndView redirect2(String userName, String note) {
+        User_MyBatis user_myBatis = new User_MyBatis();
+        user_myBatis.setUserName(userName);
+        user_myBatis.setNote(note);
+        myBatisUserService.insertUser(user_myBatis);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/user/show?id=" + user_myBatis.getId());
+        return modelAndView;
+    }
+
+    /*@GetMapping("/redirect3")
+    public String redirect3(String userName, String note, RedirectAttributes redirectAttributes) {
+        User_MyBatis user_myBatis = new User_MyBatis();
+        user_myBatis.setUserName(userName);
+        user_myBatis.setNote(note);
+        myBatisUserService.insertUser(user_myBatis);
+
+        redirectAttributes.addFlashAttribute("user", user_myBatis);
+        return "redirect:/user/showUser2";
+    }
+
+    @GetMapping("/redirect4")
+    public ModelAndView redirect4(String userName, String note, RedirectAttributes redirectAttributes) {
+        User_MyBatis user_myBatis = new User_MyBatis();
+        user_myBatis.setUserName(userName);
+        user_myBatis.setNote(note);
+        myBatisUserService.insertUser(user_myBatis);
+
+        redirectAttributes.addFlashAttribute("user", user_myBatis);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/user/showUser2");
+        return modelAndView;
+    }*/
 
 }
